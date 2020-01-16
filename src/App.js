cd08f0393cb4f2b72265492c24d1934e38a50bbf
Shawn <seungchoi2019@u.northwestern.edu>
2020-01-15 22:39:44 -0600

    fourth commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'rbx/index.css';
 import { Button, Container, Title } from 'rbx';
+import {useState} from 'react';
 
 
 const schedule = {
@@ -55,12 +56,26 @@ const CourseList = ({ courses }) => (
   </Button.Group>
 );
 
-const App = () =>  (
-  <Container>
-    <Banner title={ schedule.title } />
-    <CourseList courses={ schedule.courses } />
-  </Container>
-);
+const App = () =>  {
+  const [schedule, setSchedule] = useState({title:'',courses:[]});
+  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
+
+  React.useEffect(() => {
+    const fetchSchedule = async () => {
+      const response = await fetch(url);
+      if (!response.ok) throw response;
+      const json = await response.json();
+      setSchedule(json);
+    }
+    fetchSchedule();
+  }, [])
+  return (
+    <Container>
+      <Banner title={ schedule.title } />
+      <CourseList courses={ schedule.courses } />
+    </Container>
+  );
+};
 
 
 export default App;
